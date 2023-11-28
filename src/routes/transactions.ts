74610fc8be7a3482d1b4fcd89573852ea64a1356
emerson marinho <emerson_marinhoaz@hotmail.com>
2023-11-28 12:52:49 -0300

    feat: get all transactions and get transactions by ID

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,24 @@ import { knex } from '../database'
 import { z } from 'zod'
 
 export async function transcationsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transactions = await knex('transactions').where({ id }).first()
+
+    return { transactions }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
